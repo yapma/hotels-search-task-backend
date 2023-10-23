290886648f3e5aa8d0e58f427238c330b836f63a
yapma <yapmayashar@gmail.com>
2023-10-23 22:29:57 +0330

    - add logs entities in domain, done. - add logs contract in domain, done.

---
+++ HotelsSearchTaskBackend/src/Core/Domain/Core.Domain/Contracts/Repositories/ILogsRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain.Contracts.Repositories
+{
+    public interface ILogsRepository
+    {
+        Task AddRestApiRequestResponseLog(RestApiRequestResponse logModel);
+        Task AddExceptionLog(ExceptionLog logModel);
+        Task<List<RestApiRequestResponse>> GetAllRestApiRequestResponseLog();
+        Task<List<ExceptionLog>> GetAllExceptionLog();
+    }
+}
---
+++ HotelsSearchTaskBackend/src/Core/Domain/Core.Domain/Entities/RestApiRequestResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain.Entities
+{
+    public class RestApiRequestResponse
+    {
+        public long Id { get; set; }
+        public string? UserId { get; set; }
+        public string UrlPath { get; set; }
+        public string RequestHeader { get; set; }
+        public string? RequestBody { get; set; }
+        public string? RequestQueryStrings { get; set; }
+        public string ResponseHeader { get; set; }
+        public string? ResponseBody { get; set; }
+        public int HttpStatusCode { get; set; }
+        public long DurationInMiliSecond { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
