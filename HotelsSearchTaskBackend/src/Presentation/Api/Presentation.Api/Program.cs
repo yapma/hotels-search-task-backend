cc938c4f935181432f8dd4d86fdf23ac0f7879f4
yapma <yapmayashar@gmail.com>
2023-10-27 17:31:39 +0330

    - fix program, add cors allow.

--- HotelsSearchTaskBackend/src/Presentation/Api/Presentation.Api/Program.cs
+++ HotelsSearchTaskBackend/src/Presentation/Api/Presentation.Api/Program.cs
@@ -8,6 +8,8 @@ using System.Net;
 var builder = WebApplication.CreateBuilder(args);
 
 // add services
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 builder.Services.AddControllers(options =>
 {
     options.AddDefaultResultConvention();
@@ -15,6 +17,16 @@ builder.Services.AddControllers(options =>
 builder.Services.AddApplicationServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
 builder.Services.AddPresentationApiServices();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.AllowAnyHeader();
+                          builder.AllowAnyMethod();
+                          builder.AllowAnyOrigin();
+                      });
+});
 
 //
 var app = builder.Build();
@@ -29,6 +41,7 @@ if (app.Environment.IsDevelopment())
         config.DocumentTitle = "Hotels Search";
     });
 }
+app.UseCors(MyAllowSpecificOrigins);
 app.MapControllers();
 app.UseLogMiddleware();
 app.UseExceptionHandlerMiddleware();
