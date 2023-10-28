b04f8db28189a46ce871e9380d2781647824bd60
yapma <yapmayashar@gmail.com>
2023-10-28 23:28:03 +0330

    - add unit test for infra layer, done.

---
+++ HotelsSearchTaskBackend/tests/UnitTests/Infrastructure/UnitTests.Infrastructure.Persistence/HotelsRepositoryUnitTests.cs
@@ -0,0 +1,132 @@
+﻿using Core.Domain.Contracts.Repositories;
+using Core.Domain.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTests.Infrastructure.Persistence.Mocks;
+
+namespace UnitTests.Infrastructure.Persistence
+{
+    public class HotelsRepositoryUnitTests
+    {
+        private readonly Mock<IHotelsRepository> _repository;
+
+        public HotelsRepositoryUnitTests()
+        {
+            _repository = MockHotelsRepository.GetMock();
+        }
+
+        [Fact]
+        public async Task Get_NonParameter_ReturnsAllOfHotels()
+        {
+            // Act
+            var result = await _repository.Object.Get();
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Get_WithValidId_ReturnsOneHotel()
+        {
+            // Arrange
+            int id = 2;
+
+            // Act
+            var result = await _repository.Object.Get(id);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(id, result[0].Id);
+        }
+
+        [Fact]
+        public async Task Get_WithValidName_ReturnsFilteredHotels()
+        {
+            // Arrange
+            string name = "Far";
+
+            // Act
+            var result = await _repository.Object.Get(name: name);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public async Task GetById_WithValidId_ReturnsOneHotel()
+        {
+            // Arrange
+            int id = 2;
+
+            // Act
+            var result = await _repository.Object.GetById(id);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(id, result.Id);
+        }
+
+        [Fact]
+        public async Task Register_WithValidModel_AddHotelToContext()
+        {
+            // Arrange
+            int id = 5;
+            var model = new Hotel()
+            {
+                Id = id,
+                CreateDate = DateTime.Now,
+                ModifyDate = DateTime.Now,
+                Name = "Test",
+                Address = new Core.Domain.Entities.ValueObjects.Address("Iran", "Mazandaran", "Babolsar"),
+                Description = "Description 5",
+                StarsCount = 4
+            };
+
+            // Act
+            await _repository.Object.Register(model);
+            var bookObj = await _repository.Object.GetById(id);
+
+            // Assert
+            Assert.Equal(id, bookObj.Id);
+        }
+
+        [Fact]
+        public async Task Update_WithValidModel_UpdateHotelInContext()
+        {
+            // Arrange
+            int id = 1;
+            var newName = "Test";
+            var model = new Hotel()
+            {
+                Id = id,
+                Name = newName,
+            };
+
+            // Act
+            await _repository.Object.Update(model);
+            var hotelObj = await _repository.Object.GetById(id);
+
+            // Assert
+            Assert.Equal(newName, hotelObj.Name);
+        }
+
+        [Fact]
+        public async Task Delete_WithValidId_DeteteHotelFromContext()
+        {
+            // Arrange
+            int id = 4;
+
+            // Act
+            await _repository.Object.Delete(new Hotel() { Id=id});
+            var hotelObj = await _repository.Object.GetById(id);
+
+            // Assert
+            Assert.Null(hotelObj);
+        }
+    }
+}
---
+++ HotelsSearchTaskBackend/tests/UnitTests/Infrastructure/UnitTests.Infrastructure.Persistence/Mocks/MockHotelsRepository.cs
@@ -0,0 +1,97 @@
+﻿using Core.Domain.Contracts.Repositories;
+using Core.Domain.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Infrastructure.Persistence.Mocks
+{
+    internal class MockHotelsRepository
+    {
+        public static Mock<IHotelsRepository> GetMock()
+        {
+            var mock = new Mock<IHotelsRepository>();
+            var hotels = new List<Hotel>()
+            {
+                new Hotel()
+                {
+                    Id = 1,
+                    CreateDate = DateTime.Now,
+                    ModifyDate = DateTime.Now,
+                    Name = "Tehran Star",
+                    Address = new Core.Domain.Entities.ValueObjects.Address("Iran", "Tehran", "Tehran"),
+                    Description = "Description 1",
+                    StarsCount = 3
+                },
+                new Hotel()
+                {
+                    Id = 2,
+                    CreateDate = DateTime.Now,
+                    ModifyDate = DateTime.Now,
+                    Name = "Fardis",
+                    Address = new Core.Domain.Entities.ValueObjects.Address("Iran", "Mazandaran", "Babolsar"),
+                    Description = "Description 2",
+                    StarsCount = 4
+                },
+                new Hotel()
+                {
+                    Id = 3,
+                    CreateDate = DateTime.Now,
+                    ModifyDate = DateTime.Now,
+                    Name = "Fariz",
+                    Address = new Core.Domain.Entities.ValueObjects.Address("Iran", "Mazandaran", "Babolsar"),
+                    Description = "Description 2",
+                    StarsCount = 4
+                },
+                new Hotel()
+                {
+                    Id = 4,
+                    CreateDate = DateTime.Now,
+                    ModifyDate = DateTime.Now,
+                    Name = "Test",
+                    Address = new Core.Domain.Entities.ValueObjects.Address("Iran", "Mazandaran", "Babolsar"),
+                    Description = "Description 2",
+                    StarsCount = 4
+                }
+            };
+
+            mock.Setup(m => m.GetById(It.IsAny<int>()))
+                .ReturnsAsync((int id) => hotels.SingleOrDefault(x => x.Id == id));
+
+            mock.Setup(m => m.Get(It.IsAny<int>(), It.IsAny<string>()))
+                .ReturnsAsync((int id, string name) =>
+                {
+                    if (id == 0 && string.IsNullOrEmpty(name))
+                    {
+                        return hotels;
+                    }
+                    return hotels.Where(x => (id == 0 || x.Id == id)
+                        && (string.IsNullOrEmpty(name) || x.Name.Contains(name)))
+                        .Select(x => x)
+                        .ToList();
+                });
+
+            mock.Setup(m => m.Register(It.IsAny<Hotel>()))
+                .Callback((Hotel model) => { hotels.Add(model); });
+
+            mock.Setup(m => m.Delete(It.IsAny<Hotel>()))
+                .Callback((Hotel model) => {
+                    var hotel = hotels.Find(x => x.Id == model.Id);
+                    if(hotel != null)
+                        hotels.Remove(hotel);
+                });
+
+            mock.Setup(m => m.Update(It.IsAny<Hotel>()))
+                .Callback((Hotel model) => {
+                    var hotel = hotels.Find(x => x.Id == model.Id);
+                    if(hotel != null)
+                        hotel.Name = model.Name;
+                });
+
+            return mock;
+        }
+    }
+}
